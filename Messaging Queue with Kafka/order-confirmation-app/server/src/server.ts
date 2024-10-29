5ee67a97b01bf15c28a09e288f2452b5ea4f1b56
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-29 22:31:07 +0530

    Add orderRoutes to handle order creation

--- Messaging Queue with Kafka/order-confirmation-app/server/src/server.ts
+++ Messaging Queue with Kafka/order-confirmation-app/server/src/server.ts
@@ -2,14 +2,18 @@ import express, { Application, Request, Response } from "express";
 import { config } from "./config/config";
 import cors from "cors";
 
+import orderRoutes from "./routes/orderRoutes";
+
 const app = express();
+
+const PORT = config.port;
 
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const PORT = config.port;
+app.use("/api", orderRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
